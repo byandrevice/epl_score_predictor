8d517d9927ffd40b3806c8972391f55f50ff45ef
GiaDo2209 <dinhd2541@gmail.com>
2026-07-09 14:38:33 -0400

    update leaderboardController

--- server/controllers/leaderboardController.js
+++ server/controllers/leaderboardController.js
@@ -1,6 +1,62 @@
 // Leaderboard endpoint. TODO(Gia): aggregate total points per user from Prediction.
+const Prediction = require("../models/Prediction");
 
+// Get users ranked by total points
 exports.getTop = async (req, res) => {
   // TODO: return users ranked by total points (desc)
-  res.status(501).json({ message: "getTop not implemented" });
+  try {
+    
+    const leaderboard = await Prediction.aggregate([
+
+      // Group predictions by user
+      {
+        $group: {
+          _id: "$user",
+          totalPoints: {
+            $sum: "$pointsAwarded"
+          }
+        }
+      },
+
+      {
+        $sort: {
+          totalPoints: -1
+        }
+      },
+
+      // Join with User collection
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "user"
+        }
+      },
+
+      //Convert user array into object
+      {
+        $unwind: "$user"
+      },
+
+      {
+        $project: {
+          _id: 0,
+          userId: "user._id",
+          username: "$user.username",
+          totalPoints: 1
+        }
+      }
+
+    ]);
+
+    res.status(200).json({
+      success: true,
+      leaderboard
+    });
+  
+  } catch (err) {
+    next(err);
+  }
+
 };
